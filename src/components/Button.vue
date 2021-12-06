73d7ea47eed028f09bd46e9c74aaa732a1e9e1a9
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2021-12-06 20:15:38 +0800

    завершил вёрстку кнопок

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,57 +1,93 @@
 <template>
-  <button
-    :class="[
-      'btn',
-      {
-        outlined: type === 'outlined',
-        link: type === 'link',
-      },
-    ]"
-  >
+  <button :disabled="disabled" :class="['btn', type]">
     <slot></slot>
   </button>
 </template>
 
 <script setup lang="ts">
-  defineProps<{ type?: 'outlined' | 'link' }>();
+  interface Props {
+    type?: 'outlined' | 'link';
+    disabled?: boolean;
+  }
+
+  defineProps<Props>();
 </script>
 
 <style scoped>
   .btn {
-    display: flex;
-    justify-content: center;
-    align-items: center;
     padding: 16px 32px;
     font-size: 16px;
-    background: var(--accent-color-1);
+    border: 0;
     border-radius: 5px;
-
-    line-height: 20px;
-    text-align: center;
+    background-color: var(--accent-color-1);
+    color: #ffffff;
+    line-height: normal;
     text-transform: uppercase;
     white-space: nowrap;
-
-    color: #ffffff;
-
-    border: 0;
     cursor: pointer;
+    transition: background-color 20ms ease, color 20ms ease;
+  }
+
+  .btn:hover {
+    background-color: #2248bb;
+    color: #fff;
+  }
+
+  .btn:active {
+    background-color: #3e8bff;
+    color: #fff;
+  }
+
+  .btn:disabled {
+    background-color: #a4a4a4;
+    pointer-events: none;
   }
 
   .outlined {
     border: 2px solid var(--accent-color-1);
-    background: #fff;
+    background-color: #fff;
     color: var(--accent-color-1);
+  }
+
+  .outlined:hover {
+    border-color: #2248bb;
+  }
+
+  .outlined:active {
+    border-color: #3e8bff;
+  }
+
+  .outlined:disabled {
+    color: #a4a4a4;
+    border-color: #a4a4a4;
+    background-color: transparent;
   }
 
   .link {
     display: inline-block;
     padding: 0;
     font-size: 14px;
-    background: transparent;
+    background-color: transparent;
     border-radius: 0;
     line-height: 18px;
     text-align: initial;
     text-transform: initial;
     color: var(--accent-color-1);
+  }
+
+  .link:hover {
+    text-decoration: underline;
+    color: var(--accent-color-1);
+    background-color: transparent;
+  }
+
+  .link:active {
+    color: #3e8bff;
+    background-color: transparent;
+  }
+
+  .link:disabled {
+    background-color: transparent;
+    color: #a4a4a4;
   }
 </style>
