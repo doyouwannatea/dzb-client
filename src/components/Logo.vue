065b7ecd9b2a7bd813eddfd8ad88529f2183cd5b
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2021-12-12 13:10:11 +0800

    декомпозировал Header.vue

---
+++ src/components/Logo.vue
@@ -0,0 +1,46 @@
+<template>
+  <a class="logo" href="#">
+    <span
+      :style="{
+        backgroundImage: logoIcon && `url(${logoIcon})`,
+        minWidth: `${logoWidth}px`,
+        height: `${logoHeight}px`,
+      }"
+      class="logo-icon"
+    ></span>
+    <span>Проектная деятельность <br /><small>в ИРНИТУ</small></span>
+  </a>
+</template>
+
+<script setup lang="ts">
+  defineProps<{ logoIcon?: string; logoWidth?: number; logoHeight?: number }>();
+</script>
+
+<style scoped>
+  .logo {
+    display: flex;
+    gap: 1.25rem;
+    align-items: center;
+
+    font-family: 'Geometria';
+    font-weight: bold;
+    font-size: 23px;
+    line-height: 1em;
+    text-transform: uppercase;
+    text-decoration: none;
+    color: var(--accent-color-1);
+  }
+
+  .logo-icon {
+    min-width: 36px;
+    height: 51px;
+    background: url('../assets/logo.svg') center / contain no-repeat;
+  }
+
+  .logo small {
+    text-transform: initial;
+    font-size: 0.7826em;
+    font-weight: 600;
+    line-height: 1.375em;
+  }
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="nav">
+    <ul class="nav-list">
+      <li class="nav-item active">
+        <a href="#" class="nav-link">Все проекты</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">вопрос-ответ</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">контакты</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+  .nav {
+    color: var(--text-color);
+  }
+  .nav-list {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    z-index: 1;
+    padding-left: 0;
+    height: 100%;
+  }
+
+  .nav-item {
+    height: 100%;
+    border-bottom: 0.25em solid transparent;
+  }
+
+  .nav-item.active {
+    border-bottom: 0.25em solid var(--accent-color-2);
+  }
+
+  .nav-link {
+    font-family: inherit;
+    text-decoration: none;
+    font-weight: 600;
+    font-size: 1rem;
+    line-height: 1.25em;
+    text-transform: uppercase;
+    color: inherit;
+
+    padding-left: 1.9em;
+    padding-right: 1.9em;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
