5655e548a737337d496635153a81c284bcf9bd37
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2021-12-14 00:32:21 +0800

    добавил production url в константу

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,2 @@
 export const PROD_URL = '/dzb-client/';
-export const BASE_URL =
-  process.env.NODE_ENV === 'production' ? '/dzb-client/' : '/';
+export const BASE_URL = process.env.NODE_ENV === 'production' ? PROD_URL : '/';
