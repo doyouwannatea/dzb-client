bb9c9fbb65bf0a1fdfc5fb87fe3914de4e03cea8
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2021-12-19 03:49:52 +0800

    добавил ручки для получения значений фильтров

--- src/api/ProjectApi/ProjectApi.ts
+++ src/api/ProjectApi/ProjectApi.ts
@@ -1,8 +1,20 @@
-import { Project } from '@/models/Project';
+import { Project, SupervisorName, Tag, Type } from '@/models/Project';
 import ProjectApiType from './ProjectApiType';
 
 export default class ProjectApi extends ProjectApiType {
   static async getProjectList(page: number): Promise<Project[]> {
-    return this.ky.get('projects', { searchParams: { page } }).json();
+    return this.ky.get('api/projects', { searchParams: { page } }).json();
+  }
+
+  static async getAllTags(): Promise<Tag[]> {
+    return this.ky.get('api/tags').json();
+  }
+
+  static async getAllSupervisorNames(): Promise<SupervisorName[]> {
+    return this.ky.get('api/supervisors/names').json();
+  }
+
+  static async getAllProjectTypes(): Promise<Type[]> {
+    return this.ky.get('api/types').json();
   }
 }
--- src/api/ProjectApi/ProjectApiType.ts
+++ src/api/ProjectApi/ProjectApiType.ts
@@ -2,7 +2,7 @@ import ky from 'ky';
 import type { Project, SupervisorName, Tag, Type } from '@/models/Project';
 
 export default abstract class ProjectApiType {
-  protected static BASE_URL = 'https://projects.tw1.ru/api';
+  protected static BASE_URL = 'https://projects.tw1.ru';
   protected static ky = ky.create({ prefixUrl: ProjectApiType.BASE_URL });
 
   static async getProjectList(page: number): Promise<Project[]> {
