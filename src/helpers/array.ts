d46ea0ff349bf42da322e91f179bbd39ab536886
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2021-12-23 20:53:09 +0800

    убираю дупликаты значений массивов из поисковой строки

--- src/helpers/array.ts
+++ src/helpers/array.ts
@@ -2,8 +2,12 @@
 // 1. Сортировка
 // 2. Превращение в JSON
 // 3. сравнение строк
-export function compareArrays(array1?: any[], array2?: any[]): boolean {
+export function compareArrays(array1?: unknown[], array2?: unknown[]): boolean {
   if (!array1 && !array2) return true;
   if (!array1 || !array2) return false;
   return JSON.stringify(array1.sort()) === JSON.stringify(array2.sort());
+}
+
+export function removeDuplicates<T>(array: T[]): T[] {
+  return [...new Set(array)];
 }
