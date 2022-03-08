2677c7c742e8e5cd9359590b437b344fb066a642
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-03-08 21:00:07 +0800

    + user's projects page

---
+++ src/components/UserProjects.vue
@@ -0,0 +1,10 @@
+<template>
+  <ProjectList :project-list="projectList" />
+</template>
+
+<script setup lang="ts">
+  import ProjectList from '@/components/ProjectList.vue';
+  import { projectListResponse } from '@/models/mock/project';
+
+  const projectList = projectListResponse.data.slice(0, 2);
+</script>
