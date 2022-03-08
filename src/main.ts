1cddb5afdb508dfbb376578481d8239f73d1bdb2
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-03-08 21:51:33 +0800

    добавил либу для сохранения данных в localStorage

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import App from './App.vue';
 import { router } from './router';
 
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
 const app = createApp(App);
 app.use(router);
-app.use(createPinia());
+app.use(pinia);
 app.mount('#app');
