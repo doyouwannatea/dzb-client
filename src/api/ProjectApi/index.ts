6d2d65d718b5118cc093fd5a68d5770087b0ffc7
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-03-12 01:46:01 +0800

    обновил .env * добавил VITE_MOCK_API env вместо proccess.env.mode

--- src/api/ProjectApi/index.ts
+++ src/api/ProjectApi/index.ts
@@ -1,4 +1,6 @@
 import ProjectApi from './ProjectApi';
 import ProjectApiMock from './ProjectApiMock';
 
-export default import.meta.env.MODE === 'mock' ? ProjectApiMock : ProjectApi;
+export default import.meta.env.VITE_MOCK_API === 'true'
+  ? ProjectApiMock
+  : ProjectApi;
