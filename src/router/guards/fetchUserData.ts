52c71100628d392736eaaa4d9822a78fd4470675
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-04-18 15:29:33 +0800

    bug fix: запрос пользователя на любой странице запрашиваю данные только если редирект произошел извне

--- src/router/guards/fetchUserData.ts
+++ src/router/guards/fetchUserData.ts
@@ -3,7 +3,7 @@ import { useAuthStore } from '@/stores/auth/useAuthStore';
 
 export const fetchUserData: NavigationGuard = async (to, from, next) => {
   const authStore = useAuthStore();
-  if (!authStore.isAuth) {
+  if (!authStore.isAuth && !from.name) {
     await authStore.fetchUserData();
   }
   return next();
