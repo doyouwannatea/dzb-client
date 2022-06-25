5102537b3625cf6de729f4d836d895e153654ae6
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-06-25 12:11:00 +0800

    🆕 сообщение если сервер перегружен

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import { createApp, markRaw } from 'vue';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import Toast, { PluginOptions, POSITION } from 'vue-toastification';
 import App from './App.vue';
 import { router } from './router';
 
 const pinia = createPinia();
 const app = createApp(App);
 
+const toastOptions: PluginOptions = {
+  position: POSITION.BOTTOM_RIGHT,
+};
+
 pinia.use(piniaPluginPersistedstate);
 pinia.use(({ store }) => {
   store.router = markRaw(router);
 });
+app.use(Toast, toastOptions);
 app.use(router);
 app.use(pinia);
 app.mount('#app');
