11b1ac8db40d19a1ec90c8cc89662a02536d126d
Alexander <91420542+Sadeal@users.noreply.github.com>
2022-10-18 22:29:29 +0800

    Изменил вывод сообщения, если ничего не найдено

---
+++ src/components/project/ProjectSearchBad.vue
@@ -0,0 +1,34 @@
+<template>
+  <BaseStub :title="title" :subtitle="subtitle">
+    <template #button>
+      <BaseButton
+        is="router-link"
+        case="uppercase"
+        :to="{ name: RouteNames.HOME }"
+        @click="
+          clearFilter();
+          modalsStore.projectFilterModal = false;
+        "
+      >
+        {{ btn }}
+      </BaseButton>
+    </template>
+  </BaseStub>
+</template>
+
+<script setup lang="ts">
+  import { RouteNames } from '@/router/types/route-names';
+  import { useProjectFilters } from '@/hooks/useProjectFilters';
+  import { useModalsStore } from '@/stores/modals/useModalsStore';
+  // components
+  import BaseStub from '@/components/ui/BaseStub.vue';
+  import BaseButton from '@/components/ui/BaseButton.vue';
+
+  const { clearFilter } = useProjectFilters();
+  const modalsStore = useModalsStore();
+
+  const title = 'Проекты не найдены :(';
+  const subtitle =
+    'Пока нет ни одного проекта с введённым названием и/или выбранными фильтрами';
+  const btn = 'сбросить фильтры';
+</script>
