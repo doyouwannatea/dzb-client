804ea1a93c7874737cf860ffc9b341266bad7ad6
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-10-19 23:06:33 +0800

    🆕 добавил анимацию загрузки карточки проекта

--- src/components/project/ProjectCardEmpty.vue
+++ src/components/project/ProjectCardEmpty.vue
@@ -4,8 +4,32 @@
 
 <style lang="scss" scoped>
   .card {
+    position: relative;
     height: 29.0625rem;
     border-radius: 0.625rem;
     background: var(--gray-color-1);
+    overflow: hidden;
+
+    &::before {
+      content: '';
+      position: absolute;
+      height: 200%;
+      width: 10%;
+      background-color: var(--light-color);
+      opacity: 0.2;
+      top: -20%;
+      transform: rotate(20deg);
+      animation: translate 1100ms linear infinite;
+      filter: blur(10px);
+    }
+  }
+
+  @keyframes translate {
+    0% {
+      left: -40%;
+    }
+    100% {
+      left: 100%;
+    }
   }
 </style>
