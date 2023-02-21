c5ce9694af179e52d15c9da0891b265adc367f32
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2023-02-21 20:29:11 +0800

    🚧 UnderstandModal -> AlertModal, правки текста в модальных окнах

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <AuthModal />
-  <SelectedProjectModal />
   <ParticipationModal />
   <ParticipationSuccessModal />
   <ProjectFeedbackModal />
-  <UnderstandModal />
+  <AlertModal />
   <ToUpButton />
   <RouterView></RouterView>
 </template>
@@ -12,12 +11,11 @@
 <script setup lang="ts">
   import { RouterView } from 'vue-router';
   import AuthModal from './components/layout/AuthModal.vue';
-  import SelectedProjectModal from './components/layout/SelectedProjectModal.vue';
   import ParticipationModal from './components/participation/ParticipationModal.vue';
   import ParticipationSuccessModal from './components/participation/ParticipationSuccessModal.vue';
   import ProjectFeedbackModal from './components/feedback/ProjectFeedbackModal.vue';
   import ToUpButton from './components/ui/ToUpButton.vue';
-  import UnderstandModal from './components/layout/UnderstandModal.vue';
+  import AlertModal from './components/layout/AlertModal.vue';
   import { useSavedRoute } from './hooks/useSaveRoute';
   import { useGetParticipationList } from './hooks/useFetchStudentData';
   import { useGetParticipationDeadline } from './hooks/useGetParticipationDeadline';
