7e5671fa7cfe70db60149280acb7a344f4e0d0ab
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2023-03-11 15:34:57 +0800

    🚧 рефакторинг ProjectDetails

---
+++ src/pages/ProjectPage/ProjectDesktopDetails.vue
@@ -0,0 +1,154 @@
+<template>
+  <!-- Panel -->
+  <BasePanel>
+    <GridLayout cols="4fr 4fr 2fr">
+      <!-- Information list -->
+      <AppList
+        :items="[
+          {
+            title: 'Руководители проекта',
+            content: project.supervisorsNames || project.supervisors.join(', '),
+          },
+          {
+            title: 'Старт проекта',
+            content: project.date_start,
+          },
+          {
+            title: 'Сложность',
+            content: DifficultyText[project.difficulty],
+          },
+          {
+            title: 'Заказчик',
+            content: project.customer,
+          },
+        ]"
+      />
+      <!-- Information list -->
+      <AppList>
+        <AppListItem>
+          <template #title>Цель проекта</template>
+          <template #default>
+            {{ project.goal }}
+          </template>
+        </AppListItem>
+
+        <AppListItem>
+          <template #title>Версии проекта</template>
+          <template #default>
+            <ProjectHistoryButton
+              :history-length="history?.length"
+              @click="emit('showHistory')"
+            />
+          </template>
+        </AppListItem>
+      </AppList>
+
+      <div>
+        <p class="info-title">Статус проекта</p>
+        <ProjectStatus class="badge mt-2" :state="project.state" />
+        <p class="info-title mt-4">Максимальное количество студентов</p>
+        <ProjectTeamCounter class="mt-2" :total="project.places" />
+        <OpenParticipationModalButton class="mt-4" :project="project" />
+        <OpenFeedbackModalButton class="mt-4" :project="project" />
+      </div>
+    </GridLayout>
+  </BasePanel>
+
+  <!-- Panel -->
+  <BasePanel>
+    <!-- Information list -->
+    <AppList
+      :items="[
+        {
+          title: 'Требования к студентам',
+          content: project.requirements,
+          wide: true,
+        },
+        {
+          title: 'Описание проекта',
+          content: project.description,
+          wide: true,
+        },
+      ]"
+    />
+  </BasePanel>
+
+  <!-- Panel -->
+  <BasePanel>
+    <GridLayout :cols="2">
+      <!-- Information list -->
+      <AppList
+        :items="[
+          {
+            title: 'Ожидаемый продуктовый результат',
+            content: project.product_result,
+          },
+        ]"
+      />
+
+      <!-- Information list -->
+      <AppList
+        :items="[
+          {
+            title: 'Ожидаемый учебный результат',
+            content: project.study_result,
+          },
+        ]"
+      />
+    </GridLayout>
+  </BasePanel>
+
+  <!-- Panel -->
+  <BasePanel v-if="project?.skills.length > 0">
+    <!-- Information list -->
+    <AppList>
+      <AppListItem :bold="false" :wide="true">
+        <template #title>Теги</template>
+        <template #default>
+          <SkillList :skills="project.skills" show-all />
+        </template>
+      </AppListItem>
+    </AppList>
+  </BasePanel>
+</template>
+
+<script setup lang="ts">
+  import { DifficultyText } from '@/models/ProjectDifficulty';
+  import { Project } from '@/models/Project';
+  // components
+  import BasePanel from '@/components/ui/BasePanel.vue';
+  import GridLayout from '@/components/ui/GridLayout.vue';
+  import AppList from '@/components/ui/AppList.vue';
+  import ProjectStatus from '@/components/project/ProjectStatus.vue';
+  import OpenParticipationModalButton from '@/components/participation/OpenParticipationModalButton.vue';
+  import OpenFeedbackModalButton from '@/components/feedback/OpenFeedbackModalButton.vue';
+  import AppListItem from '@/components/ui/AppListItem.vue';
+  import SkillList from '@/components/skill/SkillList.vue';
+  import ProjectTeamCounter from '@/components/project/ProjectTeamCounter.vue';
+  import ProjectHistoryButton from './ProjectHistoryButton.vue';
+
+  interface Props {
+    project: Project;
+    history?: Project[];
+    showHistoryModal: boolean;
+  }
+
+  interface Emits {
+    (e: 'showHistory'): void;
+  }
+
+  const props = defineProps<Props>();
+  const emit = defineEmits<Emits>();
+</script>
+
+<style lang="scss" scoped>
+  .badge {
+    width: max-content;
+  }
+
+  .info-title {
+    font-size: inherit;
+    font-weight: 600;
+    line-height: normal;
+  }
+</style>
--- src/pages/ProjectPage/ProjectDetails.vue
+++ src/pages/ProjectPage/ProjectDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <template v-if="project && !loading && !error">
+  <section v-if="project && !loading && !error">
     <ProjectHistoryModal
       v-if="history"
       :current-project-id="project.id"
@@ -15,142 +15,25 @@
       :history="history"
       @show-history="showHistoryModal = true"
     />
-    <section v-else>
-      <!-- Panel -->
-      <BasePanel>
-        <GridLayout cols="4fr 4fr 2fr">
-          <!-- Information list -->
-          <AppList
-            :items="[
-              {
-                title: 'Руководители проекта',
-                content:
-                  project.supervisorsNames || project.supervisors.join(', '),
-              },
-              {
-                title: 'Старт проекта',
-                content: project.date_start,
-              },
-              {
-                title: 'Сложность',
-                content: DifficultyText[project.difficulty],
-              },
-              {
-                title: 'Заказчик',
-                content: project.customer,
-              },
-            ]"
-          />
-          <!-- Information list -->
-          <AppList>
-            <AppListItem>
-              <template #title>Цель проекта</template>
-              <template #default>
-                {{ project.goal }}
-              </template>
-            </AppListItem>
-
-            <AppListItem>
-              <template #title>Версии проекта</template>
-              <template #default>
-                <ProjectHistoryButton
-                  :history-length="history?.length"
-                  @click="showHistoryModal = true"
-                />
-              </template>
-            </AppListItem>
-          </AppList>
-
-          <div>
-            <p class="info-title">Статус проекта</p>
-            <ProjectStatus class="badge mt-2" :state="project.state" />
-            <p class="info-title mt-4">Максимальное количество студентов</p>
-            <ProjectTeamCounter class="mt-2" :total="project.places" />
-            <OpenParticipationModalButton class="mt-4" :project="project" />
-            <OpenFeedbackModalButton class="mt-4" :project="project" />
-          </div>
-        </GridLayout>
-      </BasePanel>
-
-      <!-- Panel -->
-      <BasePanel>
-        <!-- Information list -->
-        <AppList
-          :items="[
-            {
-              title: 'Требования к студентам',
-              content: project.requirements,
-              wide: true,
-            },
-            {
-              title: 'Описание проекта',
-              content: project.description,
-              wide: true,
-            },
-          ]"
-        />
-      </BasePanel>
-
-      <!-- Panel -->
-      <BasePanel>
-        <GridLayout :cols="2">
-          <!-- Information list -->
-          <AppList
-            :items="[
-              {
-                title: 'Ожидаемый продуктовый результат',
-                content: project.product_result,
-              },
-            ]"
-          />
-
-          <!-- Information list -->
-          <AppList
-            :items="[
-              {
-                title: 'Ожидаемый учебный результат',
-                content: project.study_result,
-              },
-            ]"
-          />
-        </GridLayout>
-      </BasePanel>
-
-      <!-- Panel -->
-      <BasePanel v-if="project?.skills.length > 0">
-        <!-- Information list -->
-        <AppList>
-          <AppListItem :bold="false" :wide="true">
-            <template #title>Теги</template>
-            <template #default>
-              <SkillList :skills="project.skills" show-all />
-            </template>
-          </AppListItem>
-        </AppList>
-      </BasePanel>
-    </section>
-  </template>
+    <ProjectDesktopDetails
+      v-else
+      :project="project"
+      :show-history-modal="showHistoryModal"
+      :history="history"
+      @show-history="showHistoryModal = true"
+    />
+  </section>
 </template>
 
 <script setup lang="ts">
   import { ref, watch } from 'vue';
   import { storeToRefs } from 'pinia';
-  import { DifficultyText } from '@/models/ProjectDifficulty';
   import { useProjectsStore } from '@/stores/projects/useProjectsStore';
   import { useMobileS } from '@/helpers/breakpoints';
   // components
-  import BasePanel from '@/components/ui/BasePanel.vue';
-  import GridLayout from '@/components/ui/GridLayout.vue';
-  import AppList from '@/components/ui/AppList.vue';
-  import ProjectStatus from '@/components/project/ProjectStatus.vue';
-  import ProjectTeamCounter from '@/components/project/ProjectTeamCounter.vue';
-  import OpenParticipationModalButton from '@/components/participation/OpenParticipationModalButton.vue';
-  import OpenFeedbackModalButton from '@/components/feedback/OpenFeedbackModalButton.vue';
-  import AppListItem from '@/components/ui/AppListItem.vue';
-  import SkillList from '@/components/skill/SkillList.vue';
   import ProjectMobileDetails from './ProjectMobileDetails.vue';
   import ProjectHistoryModal from '@/components/project/ProjectHistoryModal.vue';
-  import ProjectHistoryButton from './ProjectHistoryButton.vue';
+  import ProjectDesktopDetails from './ProjectDesktopDetails.vue';
 
   const isMobileS = useMobileS();
   const projectsStore = useProjectsStore();
@@ -168,17 +51,3 @@
     { deep: true },
   );
 </script>
-
-<style lang="scss" scoped>
-  @import '@styles/breakpoints';
-
-  .badge {
-    width: max-content;
-  }
-
-  .info-title {
-    font-size: inherit;
-    font-weight: 600;
-    line-height: normal;
-  }
-</style>
