06e8f73add6c62dc0576809c6648050ccc3984dc
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2023-03-19 14:13:26 +0800

    🆕 компонент "label" для контролов

---
+++ src/components/ui/BaseLabel.vue
@@ -0,0 +1,39 @@
+<template>
+  <label :class="{ disabled: $style.disabled }">
+    <p :class="$style['label-text']">
+      {{ props.label }}
+      <span v-if="props.required" :class="$style.required">*</span>
+    </p>
+    <slot></slot>
+  </label>
+</template>
+
+<script setup lang="ts">
+  type Props = {
+    label: string;
+    required?: boolean;
+    disabled?: boolean;
+  };
+
+  const props = withDefaults(defineProps<Props>(), {
+    required: false,
+    disabled: false,
+  });
+</script>
+
+<style module lang="scss">
+  .label-text {
+    margin-bottom: 0.625rem;
+    font-size: 1.125rem;
+    font-weight: 700;
+  }
+
+  .required {
+    font-weight: bold;
+    color: var(--red-color-1);
+  }
+
+  .disabled {
+    color: var(--gray-color-2);
+  }
+</style>
