e379e522a0f9385b61aed9642a05821f59c70c82
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2023-03-23 22:32:54 +0800

    🚧 обновил интерфейс Specialty

--- src/models/Specialty.ts
+++ src/models/Specialty.ts
@@ -1,7 +1,29 @@
 import { Institute } from './Institute';
 import { Tag } from './Tag';
 
-export interface Specialty extends Tag {
+export const enum SpecialtyCourse {
+  First = 1,
+  Second = 2,
+  Third = 3,
+  Fourth = 4,
+  Fifth = 5,
+}
+
+export const SpecialtyName: Record<SpecialtyCourse, string> = {
+  [SpecialtyCourse.First]: '1 курс',
+  [SpecialtyCourse.Second]: '2 курс',
+  [SpecialtyCourse.Third]: '3 курс',
+  [SpecialtyCourse.Fourth]: '4 курс',
+  [SpecialtyCourse.Fifth]: 'специалитет',
+};
+
+export interface Specialty<T = number> extends Tag<T> {
   institute?: Institute | null;
   department?: Institute | null;
+}
+
+export interface SelectedSpecialty<T = number> extends Tag<T> {
+  specialty_id: number;
+  course: SpecialtyCourse;
+  priority?: number;
 }
